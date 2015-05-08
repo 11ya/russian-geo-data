dc3525a711b83f4f0697381d8dfc7d1a2e485200
Kreozot <zzxyyz@gmail.com>
2015-05-08 14:48:28 +0300

    Full cycle of generating geojson, topojson and svg.

--- main.js
+++ main.js
@@ -3,48 +3,13 @@ var fs = require('fs');
 var topojson = require('topojson');
 var d3 = require('d3');
 var jsdom = require('jsdom');
+var SVGO = require('svgo');
 
-function saveSimplifiedTopoJson(data, proportion) {
-	var options = {'retain-proportion': proportion, 'verbose': false};
-	var topology = topojson.topology({collection: data}, options);
-	topojson.simplify(topology, options);
-	generateSvg(topology, './map-' + proportion + '.svg');
+svgo = new SVGO({
 
-	fs.writeFile('./topo-' + proportion + '.json', JSON.stringify(topology), function(err) {
-		if (err) {
-			return console.log('Error: ' + err);
-		}
-		console.log('TopoJSON ' + proportion + ': ok');
-	});
-}
+});
 
-function cloneData(data) { //Necessary for topojson.topology, because it edit input data
-	return JSON.parse(JSON.stringify(data));
-}
-
-function toTopoJson() {
-	fs.readFile('./geo.json', 'utf8', function (err, data) {
-		if (err) {
-			return console.log('Error: ' + err);
-		}
-
-		data = JSON.parse(data);
-		var options = {'verbose': false};
-		var topology = topojson.topology({collection: cloneData(data)}, options);
-
-		fs.writeFile('./topo.json', JSON.stringify(topology), function(err) {
-			if (err) {
-				return console.log('Error: ' + err);
-			}
-			console.log('TopoJSON: ok');
-		});
-
-		saveSimplifiedTopoJson(cloneData(data), 0.1);
-		saveSimplifiedTopoJson(cloneData(data), 0.2);
-		saveSimplifiedTopoJson(cloneData(data), 0.3);
-	});
-}
-toTopoJson();
+toTopoJson('./geo.json');
 
 function toGeoJson(toTopoJson) {
 	ogr2ogr('./shapefile/RUS_adm1.shp')
@@ -54,46 +19,118 @@ function toGeoJson(toTopoJson) {
 		.pipe(fs.createWriteStream('./geo.json').on('close', function() {
 			console.log('GeoJSON: ok');
 
-			toTopoJson();
+			toTopoJson('./geo.json');
 		}));	
 }
 
-function generateSvg(map, filename) {
+function cloneData(data) { //Necessary for topojson.topology, because it has to edit input data
+	return JSON.parse(JSON.stringify(data));
+}
+
+function toTopoJson(filename) {
+	fs.readFile(filename, 'utf8', function (err, data) {
+		if (err) {
+			return console.log('Error: ' + err);
+		}
+
+		data = JSON.parse(data);
+		var options = {'verbose': false};
+		var topology = topojson.topology({collection: cloneData(data)}, options);
+
+		simplifyAndSaveSvg(cloneData(data), 1);
+		simplifyAndSaveSvg(cloneData(data), 0.1);
+		simplifyAndSaveSvg(cloneData(data), 0.2);
+		simplifyAndSaveSvg(cloneData(data), 0.3);
+	});
+}
+
+function simplifyAndSaveSvg(data, proportion) {
+	simplifyTopoJson(data, proportion, function(topology) {
+		var fileSuffix = proportion == 1 ? "" : "-" + proportion;
+
+		saveTopoJson(topology, './topo' + fileSuffix + '.json');
+
+		generateSvg(topology, proportion, function(data) {
+			saveSvg(data, './map' + fileSuffix + '.svg');
+		});
+	});
+}
+
+function simplifyTopoJson(data, proportion, callback) {
+	var options = {'retain-proportion': proportion, 'verbose': true};
+	var topology = cloneData(topojson.topology({collection: data}, options));
+	topology = topojson.simplify(topology, options);
+	console.log('Simplify to ' + proportion + ': ok');
+	callback(topology);
+}
+
+function saveTopoJson(topology, filename) {
+	fs.writeFile(filename, JSON.stringify(topology), function(err) {
+		if (err) {
+			return console.log('Error: ' + err);
+		}
+		console.log('TopoJSON ' + filename + ': ok');
+	});
+}
+
+function generateSvg(topology, callback) {
 	jsdom.env('<html><body></body></html>', [], function(err, window) {
 		console.log("generating svg...");
-
+		
 		var width = 1000,
 			height = 500;
+
+		d3.select("body")
+			.selectAll("svg")
+			.remove();
 
 		var svg = d3.select("body")
 	        .append("svg")
 	        .attr("width", width)
-	        .attr("height", height);
+	        .attr("height", height)
+	        .attr("xmlns", 'http://www.w3.org/2000/svg')
+	        .attr("xmlns:xlink", 'http://www.w3.org/1999/xlink');
 
-			var projection = d3.geo.albers()
-				.rotate([-105, 0])
-				.center([-10, 65])
-				.parallels([52, 64])
-				.scale(700)
-				.translate([width / 2, height / 2]);
+	    var styleDef = svg.append('defs')
+	    	.append('style')
+	    	.text('path {stroke: #272822; stroke-width: 1px; fill-opacity: 0.8; fill: #C0C0C0;} ' +
+	    		'path:hover {fill-opacity: 1;}');
 
-			var path = d3.geo.path().projection(projection);
+		var projection = d3.geo.albers()
+			.rotate([-105, 0])
+			.center([-10, 65])
+			.parallels([52, 64])
+			.scale(700)
+			.translate([width / 2, height / 2]);
 
-			svg.append("g")
-				.selectAll("path")
-				.data(topojson.feature(map, map.objects.collection).features)
-				.enter()
-				.append("path")
-				.attr("d", path)
-				.attr("class", "region");
+		var path = d3.geo.path().projection(projection);
 
-		    fs.writeFile(filename, d3.select("body").html(), function(err) {
-				if (err) {
-					return console.log('Error: ' + err);
-				}
-				console.log('svg ' + filename + ': ok');
-			});
+		svg.append("g")
+			.selectAll("path")
+			.data(topojson.feature(topology, topology.objects.collection).features)
+			.enter()
+			.append("path")
+			.attr("d", path)
+			.attr("class", "region");
 
+		var svgData = d3.select("body").html();
+
+		optimizeSvg(svgData, callback);
 	});
 }
-// generateSvg('./topo-0.1.json');
+
+function optimizeSvg(data, callback) {
+	console.log("optimizing svg...");
+	svgo.optimize(data, function(result) {
+		callback(result.data);
+    });
+}
+
+function saveSvg(data, filename) {
+    fs.writeFile(filename, data, function(err) {
+		if (err) {
+			return console.log('Error: ' + err);
+		}
+		console.log('svg ' + filename + ': ok');
+	});
+}
